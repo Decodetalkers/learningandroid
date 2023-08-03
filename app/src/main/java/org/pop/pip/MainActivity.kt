9a78b403694fd3f52b8d15f25f269fb091735b34
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-08-03 21:56:44 +0800

    chore: finishe navigate

--- app/src/main/java/org/pop/pip/MainActivity.kt
+++ app/src/main/java/org/pop/pip/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -15,33 +16,47 @@ import org.pop.pip.ui.theme.PopAndPipTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MainUi(name = "ssscc") }
+        setContent { TopUi() }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column {
-        Text("$name is ")
-        Text("$name is ")
-        Button(onClick = { println("ss") }) { Text("beta2") }
-        Text("$name is ")
-        Text("$name is ")
-    }
-}
-
-@Composable
-fun MainUi(name: String) {
+fun TopUi() {
     PopAndPipTheme {
         val navController = rememberNavController()
         NavHost(navController = navController, startDestination = "login") {
-            composable("login") { Greeting(name) }
+            composable("login") { LoginPage(navController) }
+            composable("context") { SecondPage(navController) }
         }
+    }
+}
+
+@Composable
+fun LoginPage(navController: NavController) {
+    var name = "sssss"
+    Column {
+        Text("$name is ")
+        Text("$name is ")
+        Button(onClick = { navController.navigate("context") }) { Text("To Second One") }
+        Text("$name is ")
+        Text("$name is ")
+    }
+}
+
+@Composable
+fun SecondPage(navController: NavController) {
+    var name = "eeee"
+    Column {
+        Text("$name is ")
+        Text("$name is ")
+        Button(onClick = { navController.navigate("login") }) { Text("to Main Screen") }
+        Text("$name is ")
+        Text("$name is ")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    PopAndPipTheme { Greeting("Android") }
+    PopAndPipTheme {}
 }
