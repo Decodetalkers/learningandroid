9f6d8c482ec192b0e89cd6361d9e5408207c48ba
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-02 17:47:06 +0800

    chore: add preview

--- app/src/main/java/org/pop/pip/MainActivity.kt
+++ app/src/main/java/org/pop/pip/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -228,6 +229,7 @@ fun SearchResultPage(
     }
 }
 
+@Preview
 @Composable
 fun FloatActionBtn() {
     ExtendedFloatingActionButton(
